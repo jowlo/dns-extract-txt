9e783b608b32badf105b7595cc3bb6d571933dcd
Jonas Wloka <jowlo@uni-bremen.de>
2014-03-31 19:25:45 +0200

    remove old copyright, shorter imports

--- src/DNSQuery.java
+++ src/DNSQuery.java
@@ -1,16 +1,3 @@
-
-
-/*
- * Java Network Programming, Second Edition
- * Merlin Hughes, Michael Shoffner, Derek Hamner
- * Manning Publications Company; ISBN 188477749X
- *
- * http://nitric.com/jnp/
- *
- * Copyright (c) 1997-1999 Merlin Hughes, Michael Shoffner, Derek Hamner;
- * all rights reserved; see license.txt for details.
- */
-
 import java.io.*;
 import java.util.*;
 
--- src/ExtractTXT.java
+++ src/ExtractTXT.java
@@ -1,11 +1,4 @@
-
-
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class ExtractTXT {
