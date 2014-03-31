fb797a22fc16698c9b174299e8c24ee5debab659
Jonas Wloka <jowlo@uni-bremen.de>
2014-03-31 19:10:43 +0200

    initial commit

---
+++ src/DNSInputStream.java
@@ -0,0 +1,67 @@
+
+
+/*
+ * Java Network Programming, Second Edition
+ * Merlin Hughes, Michael Shoffner, Derek Hamner
+ * Manning Publications Company; ISBN 188477749X
+ *
+ * http://nitric.com/jnp/
+ *
+ * Copyright (c) 1997-1999 Merlin Hughes, Michael Shoffner, Derek Hamner;
+ * all rights reserved; see license.txt for details.
+ */
+
+import java.io.*;
+
+public class DNSInputStream extends ByteArrayInputStream {
+  protected DataInputStream dataIn;
+
+  public DNSInputStream (byte[] data, int off, int len) {
+    super (data, off, len);
+    dataIn = new DataInputStream (this);
+  }
+
+  public int readByte () throws IOException {
+    return dataIn.readUnsignedByte ();
+  }
+
+  public int readShort () throws IOException {
+    return dataIn.readUnsignedShort ();
+  }
+
+  public long readInt () throws IOException {
+    return dataIn.readInt () & 0xffffffffL;
+  }
+
+  public String readString () throws IOException {
+    int len = readByte ();
+    if (len == 0) {
+      return "";
+    } else {
+      byte[] buffer = new byte[len];
+      dataIn.readFully (buffer);
+      return new String (buffer, "latin1");
+    }
+  }
+
+  public String readDomainName () throws IOException {
+    if (pos >= count) throw new EOFException ("EOF reading domain name");
+    if ((buf[pos] & 0xc0) == 0) {
+      String label = readString ();
+      if (label.length () > 0) {
+        String tail = readDomainName ();
+        if (tail.length () > 0)
+          label = label + '.' + tail;
+      }
+      return label;
+    } else {
+      if ((buf[pos] & 0xc0) != 0xc0)
+        throw new IOException ("Invalid domain name compression offset");
+      int offset = readShort () & 0x3fff;
+      DNSInputStream dnsIn = new DNSInputStream (buf, offset, buf.length - offset);
+      String domainName = dnsIn.readDomainName ();
+      dnsIn.close();
+      return domainName;
+    }
+  }
+}
---
+++ src/DNSQuery.java
@@ -0,0 +1,76 @@
+
+
+/*
+ * Java Network Programming, Second Edition
+ * Merlin Hughes, Michael Shoffner, Derek Hamner
+ * Manning Publications Company; ISBN 188477749X
+ *
+ * http://nitric.com/jnp/
+ *
+ * Copyright (c) 1997-1999 Merlin Hughes, Michael Shoffner, Derek Hamner;
+ * all rights reserved; see license.txt for details.
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class DNSQuery {
+  private String queryHost;
+  private int queryType, queryClass, queryID;
+  private static int globalID;
+
+  public DNSQuery (String host, int type, int clas) {
+    StringTokenizer labels = new StringTokenizer (host, ".");
+    while (labels.hasMoreTokens ())
+      if (labels.nextToken ().length () > 63)
+        throw new IllegalArgumentException ();
+    queryHost = host;
+    queryType = type;
+    queryClass = clas;
+    synchronized (getClass ()) {
+      queryID = (++ globalID) % 65536;
+    }
+  }
+
+  public String getQueryHost () {
+    return queryHost;
+  }
+
+  public int getQueryType () {
+    return queryType;
+  }
+
+  public int getQueryClass () {
+    return queryClass;
+  }
+
+  public int getQueryID () {
+    return queryID;
+  }
+
+  public byte[] extractQuery () {
+    ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream ();
+    DataOutputStream dataOut = new DataOutputStream (byteArrayOut);
+    try {
+      dataOut.writeShort (queryID);
+      dataOut.writeShort ((0 << 15) |
+                          (0 << 11) |
+                          (1 << 8));
+      dataOut.writeShort (1); // # queries
+      dataOut.writeShort (0); // # answers
+      dataOut.writeShort (0); // # authorities
+      dataOut.writeShort (0); // # additional
+      StringTokenizer labels = new StringTokenizer (queryHost, ".");
+      while (labels.hasMoreTokens ()) {
+        String label = labels.nextToken ();
+        dataOut.writeByte (label.length ());
+        dataOut.writeBytes (label);
+      }
+      dataOut.writeByte (0);
+      dataOut.writeShort (queryType);
+      dataOut.writeShort (queryClass);
+    } catch (IOException ignored) {
+    }
+    return byteArrayOut.toByteArray ();
+  }
+}
